7f930208593822350ddcaa77b10e0a81463f963e
Anesa <akrslak17@gmail.com>
2025-04-12 09:55:25 +0200

    Implement backend logic for login (first version).

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, send_from_directory
 
-app = Flask(__name__)
+import os
 
-@app.route("/")
-def home():
-    return "Hello from the backend!"
+app = Flask(
+    __name__,
+    static_folder="../frontend",     
+    static_url_path=""                   
+)
+
+#Login route
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if email and password:
+            return redirect("/pages/index.html")
+        else:
+            return "Morate unijeti i email i šifru!"
+
+    return send_from_directory("../frontend/pages", "Login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
